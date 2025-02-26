6bbdfd0e8677cedd6baa77a545f27ab68dda3e69
becca16 <33611387-becca16@users.noreply.replit.com>
2025-02-26 21:51:13 +0000

    User checkpoint: Remove border from CoHosts component

--- client/src/components/CoHosts.tsx
+++ client/src/components/CoHosts.tsx
@@ -9,7 +9,7 @@ const hosts = [
 
 export function CoHosts() {
   return (
-    <section className="py-20 px-4 border-2">
+    <section className="py-20 px-4">
       <motion.div
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
